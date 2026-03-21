f4d409b70e5e068d9f452365327eaec4d5629ae3
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2026-03-21 02:22:35 -0400

    Completed storage of critical variables

---
+++ RouteToGateway.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class RouteToGateway {
+        // storing critical information
+        static int n; // # of routers
+        static int[][] matrix; // directed graph
+        static int[] gateway; // Gateway router #
+        static int[] sa; // sa router #
+    public static void main(String[] args) {
+        parser();
+
+    }
+
+    static void parser() {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
