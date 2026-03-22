7851a02f0305b3a9f1c7f10a3dbb16000f84959b
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2026-03-22 01:45:40 -0400

    Completed Dijkstra's algorithm function with policy requirements

--- RouteToGateway.java
+++ RouteToGateway.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class RouteToGateway {
-        // storing critical information
+
+        // all static variables
         static int n; // # of routers
         static int[][] matrix; // directed graph
         static int[] gateways; // Gateway router #
         static int sa; // sa router #
+
     public static void main(String[] args) {
         parser();
         int[][] transpose = transposeGraph();
 
+        DijkstraRes fromSA = dijkstra(matrix, sa); // compute shortest distances from SA
+        DijkstraRes toSA = dijkstra(transpose, sa); // compute shortest distances to SA (transpose)
+
     }
+
+    // parser for directed graph
     static void parser() {
         Scanner sc = new Scanner(System.in);
 
@@ -48,5 +57,55 @@ public class RouteToGateway {
         }
 
         return transpose;
+    }
+
+    static class DijkstraRes {
+        int[] dist;
+        int[] succ;
+
+        DijkstraRes(int[] dist, int[] succ) {
+            this.dist = dist;
+            this.succ = succ;
+        }
+    }
+
+    static DijkstraRes dijkstra(int[][] dg, int start) {
+        int[] dist = new int[n + 1]; // distance array
+        int[] succ = new int[n + 1]; // successor array
+        boolean[] visited = new boolean[n + 1];
+
+        // set every distance to inf (Integer.MAX_VALUE)
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(succ, -1); // set every successor to -1
+
+        dist[start] = 0; // starting router
+        for (int count = 1; count <= n; count++) { // run dijkstra to n times
+            int u = -1;
+            int minDist = Integer.MAX_VALUE;
+
+            for (int i = 1; i <= n; i++) { // find unvisted routers
+                if (!visited[i] && dist[i] < minDist) {
+                    minDist = dist[i];
+                    u = i;
+                }
+            }
+
+            if (u == -1) {
+                break;
+            }
+
+            visited[u] = true;
+
+            for (int v = 1; v <= n; v++) { // check all adjacent routers for shorter paths
+                if (dg[u][v] != -1 && !visited[v]) {
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + dg[u][v] < dist[v]) {
+                        dist[v] = dist[u] + dg[u][v];
+                        succ[v] = u;
+                    }
+                }
+            }
+        }
+
+        return new DijkstraRes(dist, succ);
     }
 }
